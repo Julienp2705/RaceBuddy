98236d1837f3152230b2c589e3a2b54e8be82d8d
Alex Kerespars <akcodepro@gmail.com>
2026-08-26 16:08:47 +0200

    Add races#show with conditional target creation link

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,4 +2,9 @@ class RacesController < ApplicationController
   def index
     @races = Race.all
   end
+
+  def show
+    @race = Race.find(params[:id])
+    @target = Target.find_by(race: @race, user: current_user)
+  end
 end
