f643dcddcd34f455b159021c558e9f26b98c6a31
Robert Delaoutre r.delaoutre@gmail.com <r.delaoutre@gmail.com>
2026-08-27 16:39:14 +0200

    profile modification et suppression de l'objectif

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,10 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    @target = current_user.targets.last
   end
 end
+
+# def edit
+# @target = Target.find(params[:id])
+# end
--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -9,15 +9,29 @@ class TargetsController < ApplicationController
     @target = @race.targets.new(target_params)
     @target.user = current_user
 
-    if @target.save
-      redirect_to @race
-    else
-      puts @target.errors.full_messages
-      render :new, status: :unprocessable_entity
-    end
+    # if @target.save
+    # redirect_to @race
+    # else
+    # puts @target.errors.full_messages
+    # render :new, status: :unprocessable_entity
+  end
+
+  def destroy
+    @target = Target.find(params[:id])
+    @target.destroy
+
+    redirect_to profile_path
+  end
+
+  def update
+    @target = Target.find(params[:id])
+    @target.update(target_params)
+
+    redirect_to profile_path
   end
 
   private
+
   def target_params
     params.require(:target).permit(:target_hour, :target_minute)
   end
