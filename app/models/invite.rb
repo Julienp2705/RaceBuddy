e03390a6a9c0a8bc6b6dc77f42190dc7e9769af5
Laurie Lescos <laurie.oo@hotmail.fr>
2026-08-28 15:43:08 +0200

    derniere modif

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,8 +1,8 @@
 class Invite < ApplicationRecord
   belongs_to :user
   belongs_to :target
-  has_many :reviews
-  has_one :chat
+  has_many :reviews, dependent: :destroy
+  has_one :chat, dependent: :destroy
 
   scope :pending, -> { where(status: "pending") }
 end
