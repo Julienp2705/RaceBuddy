41a5314ec985e005e208c88b44382231f7692505
Laurie Lescos <laurie.oo@hotmail.fr>
2026-08-31 17:11:44 +0200

    Migrations à jour

--- db/migrate/20260831092600_add_user_and_buddy_and_rating_to_buddy_ratings.rb
+++ db/migrate/20260831092600_add_user_and_buddy_and_rating_to_buddy_ratings.rb
@@ -1,6 +1,6 @@
 class AddUserAndBuddyAndRatingToBuddyRatings < ActiveRecord::Migration[8.1]
   def change
-    remove_reference :buddy_ratings, :invite, null: false, foreign_key: true, index: { unique: true }
+    add_column :buddy_ratings, :rating, :integer,  null: false
 
     add_reference :buddy_ratings, :user,  null: false, foreign_key: { to_table: :users }
     add_reference :buddy_ratings, :buddy, null: false, foreign_key: { to_table: :users }
