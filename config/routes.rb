d35363e753fe9b9d7e134d25ec1f2bcc7af870d0
Laurie Lescos <laurie.oo@hotmail.fr>
2026-08-31 16:04:25 +0200

    Home et Races#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "races#index"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :races, only: [:create, :index, :show] do
     resources :targets, only: [:create]
