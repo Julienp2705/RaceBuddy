8b5315220424b3c2cc51cd3ffc0da3be14fc2e19
Julien Poulain julien.poulainpro@gmail.com <julien.poulainpro@gmail.com>
2026-09-01 17:28:51 +0200

    fix helper

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,8 +1,10 @@
-def race_category_label(distance)
-  case distance
-  when 0..10 then "Courte"
-  when 10..21 then "Moyenne"
-  when 21..42 then "Longue"
-  else "Ultra"
+module RacesHelper
+  def race_category_label(distance)
+    case distance
+    when 0...10 then "Courte"
+    when 10...21.1 then "Moyenne"
+    when 21.1...42.2 then "Longue"
+    else "Ultra"
+    end
   end
 end
